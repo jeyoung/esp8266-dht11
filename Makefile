6ce453fe2bb758c2b26ddb199277fe537057e565
Eddy Young <jeyoung@priscimon.com>
2020-06-28 11:48:47 +0100

    esp8266: Add networking
    * Add CA certificate for vpn.priscimon.net and adjust Makefile
    * Add first HTTP request

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 SDK_HOME = /home/pi/source/github/esp-open-sdk/sdk
 CC = $(SDK_HOME)/../xtensa-lx106-elf/bin/xtensa-lx106-elf-gcc
 CFLAGS = -I. -mlongcalls -I$(SDK_HOME)/include -I$(SDK_HOME)/driver_lib/driver -I$(SDK_HOME)/driver_lib/include/driver
-LDLIBS = -nostdlib -Wl,--start-group -lmain -lnet80211 -lwpa -llwip -lpp -lphy -lc -Wl,--end-group -lgcc -ldriver
+LDLIBS = -nostdlib -Wl,--start-group -lmain -lnet80211 -lwpa -llwip -lpp -lphy -lc -ldriver -lmbedtls -lssl -lcrypto -Wl,--end-group -lgcc
 LDFLAGS = -T$(SDK_HOME)/ld/eagle.app.v6.ld -L$(SDK_HOME)/lib
+CA_MEM_LOC = 0xAA000
 
 dht11-0x00000.bin: dht11
 	esptool.py elf2image $^
@@ -12,7 +13,7 @@ dht11: dht11.o
 dht11.o: dht11.c
 
 flash: dht11-0x00000.bin
-	esptool.py write_flash 0 dht11-0x00000.bin 0x10000 dht11-0x10000.bin
+	esptool.py write_flash 0 dht11-0x00000.bin 0x10000 dht11-0x10000.bin $(CA_MEM_LOC) esp_ca_cert.bin
 
 clean:
 	rm -f dht11 dht11.o dht11-0x00000.bin dht11-0x10000.bin
--- dht11.c
+++ dht11.c
@@ -1,17 +1,28 @@
 #include "ets_sys.h"
 #include "gpio.h"
 #include "hw_timer.c"
+#include "mem.h"
 #include "os_type.h"
 #include "osapi.h"
+#include "sntp.h"
 #include "uart.h"
 #include "user_interface.h"
+
+#include "ip_addr.h"
+#include "espconn.h"
+
 #include "wifi_credentials.h"
 
-#define T_CALIB     -3
-#define RH_CALIB    +1
+// Adjustments based on external measurements
+#define T_CALIB  -3
+#define RH_CALIB +1
 
-#define STARTING_DELAY_US    (2*1000000)
-#define PAUSE_TIME_US     (3*60*1000000) // Maxiumum is 268435455
+#define STARTING_DELAY_US   (5*1000000)
+#define SNTP_RETRY_INTERVAL (STARTING_DELAY_US/1000)
+
+// The maximum value for PAUSE_TIME_US is the same as the maximum for
+// `wifi_fpm_do_sleep(uint32)`.
+#define PAUSE_TIME_US       (30*1000000)
 
 static volatile enum
 {
@@ -32,19 +43,133 @@ static volatile enum
 static const int pin = 2;
 
 static volatile uint32_t timer_interval_us = 10;
-static volatile int reset_timer_elapsed = 0;
-static volatile int hw_timer_elapsed = 0;
+static volatile uint32_t reset_timer_elapsed = 0;
+static volatile uint32_t hw_timer_elapsed = 0;
 
 static volatile int data = 0;
 static volatile int checksum = 0;
 static volatile int data_counter = 0;
 
-static os_timer_t os_timer;
+static os_timer_t post_wakeup_timer;
+
+static volatile int post_wakeup_completed = 0;
+
+static struct espconn espconn;
+static ip_addr_t ip_addr;
+
+void pre_sleep(void)
+{
+    sntp_stop();
+    espconn_secure_disconnect(&espconn);
+}
+
+void post_wakeup_timerfunc(void *args)
+{
+
+    uint32_t current_timestamp = sntp_get_current_timestamp();
+    if (current_timestamp == 0)
+    {
+        os_timer_arm(&post_wakeup_timer, SNTP_RETRY_INTERVAL, 0);
+        post_wakeup_completed = 0;
+    }
+    else
+    {
+        os_timer_disarm(&post_wakeup_timer);
+        post_wakeup_completed = 1;
+
+        os_printf("\r\n");
+        os_printf("Unix timestamp: %d, Time (UTC): %s\r\n",
+                current_timestamp, sntp_get_real_time(current_timestamp));
+
+        os_printf("Arming HW timer for %d μs...\r\n", timer_interval_us);
+        hw_timer_arm(timer_interval_us);
+    }
+}
+
+void post_wakeup()
+{
+    sntp_setservername(0, "ntp01.algon.dk");
+    sntp_setservername(1, "ntp.uit.one");
+    sntp_setservername(2, "time.windows.com");
+    sntp_set_timezone(0);
+    sntp_init();
+
+    os_timer_disarm(&post_wakeup_timer);
+    os_timer_setfn(&post_wakeup_timer, (os_timer_func_t *)post_wakeup_timerfunc, NULL);
+    os_timer_arm(&post_wakeup_timer, SNTP_RETRY_INTERVAL, 0);
+    post_wakeup_completed = 0;
+}
+
+void ICACHE_FLASH_ATTR conn_receive_cb(void *arg, char *pdata, unsigned short len)
+{
+    struct espconn *espconn = (struct espconn *)arg;
+    os_printf("Size: %d, Received: %s\r\n", len, pdata);
+}
+
+void ICACHE_FLASH_ATTR conn_sent_cb(void *arg)
+{
+    os_printf("Sent\r\n");
+}
+
+void ICACHE_FLASH_ATTR conn_reconnect_cb(void *arg, sint8 err)
+{
+    os_printf("Reconnection status: %d\r\n", err);
+}
+
+void ICACHE_FLASH_ATTR conn_disconnect_cb(void *arg)
+{
+    os_printf("Disconnected\r\n");
+}
+
+void ICACHE_FLASH_ATTR conn_connect_cb(void *arg)
+{
+    struct espconn *espconn = (struct espconn *)arg;
+
+    espconn_regist_disconcb(espconn, conn_disconnect_cb);
+    espconn_regist_recvcb(espconn, conn_receive_cb);
+    espconn_regist_sentcb(espconn, conn_sent_cb);
+
+    char *data = "GET / HTTP/1.1\r\nHost: vpn.priscimon.net\r\nAccept:*/*\r\n\r\n";
+    sint8 result = espconn_secure_send(espconn, data, os_strlen(data));
+
+    os_printf("Send status: %d\r\n", result);
+}
+
+void ICACHE_FLASH_ATTR conn_hostfound_cb(const char *name, ip_addr_t *ip_addr, void *arg) {
+    struct espconn *espconn = (struct espconn *)arg;
+    if (ip_addr != NULL)
+    {
+        os_printf("Hostname resolved '%s':  %d.%d.%d.%d\r\n",
+                name,
+                *((uint8 *)&ip_addr->addr),     *((uint8 *)&ip_addr->addr + 1),
+                *((uint8 *)&ip_addr->addr + 2), *((uint8 *)&ip_addr->addr + 3));
+
+        espconn->type = ESPCONN_TCP;
+        espconn->state = ESPCONN_NONE;
+        espconn->proto.tcp = (esp_tcp *)os_zalloc(sizeof(esp_tcp));
+        espconn->proto.tcp->local_port = espconn_port();
+        espconn->proto.tcp->remote_port = 443;
+        os_memcpy(espconn->proto.tcp->remote_ip, &(ip_addr->addr), 4);
+
+        espconn_regist_connectcb(espconn, conn_connect_cb);
+        espconn_regist_reconcb(espconn, conn_reconnect_cb);
+
+        espconn_secure_set_size(ESPCONN_CLIENT, 8192);
+        espconn_secure_ca_enable(ESPCONN_CLIENT, 0xAA);
+        sint8 result = espconn_secure_connect(espconn);
+        os_printf("Connection status: %d\r\n", result);
+    }
+    else
+    {
+        os_printf("Cannot resolve '%s'\r\n", name);
+    }
+}
 
 void starting(void)
 {
     GPIO_OUTPUT_SET(pin, ~(GPIO_INPUT_GET(pin)));
-    if (hw_timer_elapsed > STARTING_DELAY_US)
+
+    if (hw_timer_elapsed > STARTING_DELAY_US && post_wakeup_completed)
     {
         GPIO_OUTPUT_SET(pin, 0);
         state = STARTED;
@@ -199,12 +324,14 @@ void received_data(void)
     os_printf("Checksum (calculated/supplied): %d/%d\r\n", x, checksum);
     os_printf("\r\n");
 
+    espconn_gethostbyname(&espconn, "vpn.priscimon.net", &ip_addr, conn_hostfound_cb);
+
     state = DONE;
 }
 
 void error(void)
 {
-    os_printf("ERR: %d\r\n", previous_state);
+    os_printf("Error: %d\r\n", previous_state);
     state = RESET;
 }
 
@@ -225,14 +352,14 @@ void wifi_wakeupfunc(void)
     wifi_set_opmode(STATION_MODE);
     wifi_station_connect();
 
-    os_printf("Arming HW timer for %d μs...\r\n", timer_interval_us);
-
-    hw_timer_arm(timer_interval_us);
+    post_wakeup();
 }
 
 void reset(void)
 {
     os_printf("Going in light sleep for %d ms...\r\n", PAUSE_TIME_US/1000);
+
+    pre_sleep();
 
     wifi_station_disconnect();
     wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
@@ -246,7 +373,6 @@ void done(void)
 {
     state = RESET;
 }
-
 
 void hw_timerfunc(void)
 {
@@ -299,11 +425,10 @@ void hw_timerfunc(void)
         hw_timer_arm(timer_interval_us);
 }
 
-void ICACHE_FLASH_ATTR
-user_set_station_config(void)
+void ICACHE_FLASH_ATTR start_wifi_station(void)
 {
-    wifi_set_sleep_type(MODEM_SLEEP_T);
     wifi_set_opmode(STATION_MODE);
+    wifi_fpm_set_sleep_type(MODEM_SLEEP_T);
 
     // SSID and PASSWORD must be defined in wifi_credentials.h
     char ssid[32] = SSID;
@@ -317,13 +442,16 @@ user_set_station_config(void)
     wifi_station_set_config(&stationConf);
 }
 
-void ICACHE_FLASH_ATTR user_init()
+void ICACHE_FLASH_ATTR user_init(void)
 {
     uart_init(BIT_RATE_115200, BIT_RATE_115200);
-    user_set_station_config();
+
+    start_wifi_station();
 
     gpio_init();
     PIN_PULLUP_DIS(PERIPHS_IO_MUX_GPIO2_U);
+
+    post_wakeup();
 
     hw_timer_init(FRC1_SOURCE, 0);
     hw_timer_set_func(hw_timerfunc);
